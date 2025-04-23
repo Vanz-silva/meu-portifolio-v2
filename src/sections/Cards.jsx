3d94ad7c4c90a90f2e98794d74e90863befd2186
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-04-23 01:06:15 -0300

    Comonente Cards

--- src/sections/Cards.jsx
+++ src/sections/Cards.jsx
@@ -1,50 +1,46 @@
 "use client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
+import Image from "next/image";
 const ResponsiveGrid = () => {
   return (
-    <div className="container text-center">
+    <div className="container text-center card-grid">
       <div className="row">
-        <div
-          className="col-12 col-sm-4 col-md-4"
-          style={{
-            backgroundImage: "url(/img/box-cere.png)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "300px",
-          }}
-        >
+        <div className="col-12 col-sm-4 col-md-4 col-card h-100">
           <div className="d-flex justify-content-center align-items-center h-100 text-white">
-            <p>Column 1</p>
+            <Image
+              src="/img/cerebro.png"
+              alt="img cerebro"
+              width={100}
+              height={100}
+              className="img-fluid cerebro-img m-3"
+            />
           </div>
+          <h2>Estrategia para seu negocio com inteligencia</h2>
         </div>
-        <div
-          className="col-12 col-sm-4 col-md-4"
-          style={{
-            backgroundImage: "url(/images/image2.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "300px",
-          }}
-        >
+        <div className="col-12 col-sm-4 col-md-4 col-card h-100">
           <div className="d-flex justify-content-center align-items-center h-100 text-white">
-            <p>Column 2</p>
+            <Image
+              src="/img/cerebro.png"
+              alt="img cerebro"
+              width={100}
+              height={100}
+              className="img-fluid cerebro-img m-3"
+            />
           </div>
+          <h2>Estrategia para seu negocio com inteligencia</h2>
         </div>
-        <div
-          className="col-12 col-sm-4 col-md-4"
-          style={{
-            backgroundImage: "url(/images/image3.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "300px",
-          }}
-        >
+        <div className="col-12 col-sm-4 col-md-4 col-card h-100">
           <div className="d-flex justify-content-center align-items-center h-100 text-white">
-            <p>Column 3</p>
+            <Image
+              src="/img/cerebro.png"
+              alt="img cerebro"
+              width={100}
+              height={100}
+              className="img-fluid cerebro-img m-3"
+            />
           </div>
+          <h2>Estrategia para seu negocio com inteligencia</h2>
         </div>
       </div>
     </div>
