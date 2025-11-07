ccb7791dbe899938ca9eccdc068b907a12bec34f
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-11-06 23:35:22 -0300

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/meu-portifolio",
+  assetPrefix: "/meu-portifolio/",
+};
 
 export default nextConfig;
