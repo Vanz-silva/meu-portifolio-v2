9c51410b073fd8857811c04db6a109ef06fadfa8
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-11-07 10:07:35 -0300

    trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   basePath: "/meu-portifolio-v2",
   assetPrefix: "/meu-portifolio-v2/",
+  trailingSlash: true, //  força caminhos relativos corretos
 };
 
 export default nextConfig;
