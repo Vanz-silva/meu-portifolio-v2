f80d95f2bded7dfac9dd7a81d3f31d7d69d47a60
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-11-07 19:30:26 -0300

    chore: gap

--- src/sections/Cards.jsx
+++ src/sections/Cards.jsx
@@ -18,11 +18,15 @@ const CardGrid = () => {
             title:
               "React dá vida às interfaces com interações rápidas e experiências dinâmicas.",
           },
-          { img: "/img/nodejs.png", title: "Node.js conecta tudo no backend, criando APIs velozes e escaláveis." },
+          {
+            img: "/img/nodejs.png",
+            title:
+              "Node.js conecta tudo no backend, criando APIs velozes e escaláveis.",
+          },
         ].map((card, index) => (
           <div
             key={index}
-            className="card col-card p-3 text-center"
+            className="card col-card p-3 text-center d-flex flex-colunm gap-3"
             style={{ width: "300px", flex: "1 1 280px", maxWidth: "100%" }}
           >
             <Image
@@ -30,9 +34,11 @@ const CardGrid = () => {
               alt={card.title}
               width={100}
               height={100}
-              className="img-fluid mx-auto mb-3"
+              className="img-fluid mx-auto "
             />
-            <h4 className="h5 text-white">{card.title} </h4>
+            <div>
+              <h4 className="h5 text-white">{card.title} </h4>
+            </div>
           </div>
         ))}
       </div>
