0cbafd3c5fc5e8808a43dbd86d92bf31e10c0979
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-11-07 11:19:37 -0300

    feat: novo componente home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,11 @@
 "use client";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import About from "@/sections/About";
-import Skills from "@/sections/Skills";
-import Projects from "@/sections/Projects";
-import ResponsiveGrid from "@/sections/Cards";
-import "bootstrap/dist/css/bootstrap.min.css";
-import MatrixSection from "@/components/MatrixSection";
-import Contato from "@/components/Contato";
-import Lottie from "@/components/Lottie"
+import dynamic from "next/dynamic";
 
+// Importa o componente Home dinamicamente no cliente
+const ClientHome = dynamic(() => import("@/components/HomePage"), {
+  ssr: false, //  desativa a renderização no servidor
+});
 
-export default function Home() {
-  return (
-    <>
-      <Header />
-      <main>
-        <About />
-        <Skills />
-        <Projects />
-        <MatrixSection />
-        <Lottie />
-      </main>
-      <Contato />
-      <Footer />
-    </>
-  );
+export default function Page() {
+  return <ClientHome />;
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import About from "@/sections/About";
+import Skills from "@/sections/Skills";
+import Projects from "@/sections/Projects";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MatrixSection from "@/components/MatrixSection";
+import Contato from "@/components/Contato";
+import Lottie from "@/components/Lottie";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <About />
+        <Skills />
+        <Projects />
+        <MatrixSection />
+        <Lottie />
+      </main>
+      <Contato />
+      <Footer />
+    </>
+  );
+}
